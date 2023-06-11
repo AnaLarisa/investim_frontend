fa8ad31d5ff34d0536b99b5eceabc013cddb6f76
AnaLarisa <ana.ivanov@student.upt.ro>
2023-06-12 00:56:39 +0300

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
