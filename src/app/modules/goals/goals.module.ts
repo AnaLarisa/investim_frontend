57f8eb86ff2a21c20cc901438af2211442368dcf
AnaLarisa <ana.larisa@student.upt.ro>
2023-08-24 02:09:21 +0300

    added more modules to the project

---
+++ src/app/modules/goals/goals.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GoalsComponent } from './goals/goals.component';
+
+
+
+@NgModule({
+  declarations: [
+    GoalsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GoalsModule { }
---
+++ src/app/modules/newspaper/newspaper.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NewspaperComponent } from './newspaper/newspaper.component';
+
+
+
+@NgModule({
+  declarations: [
+    NewspaperComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class NewspaperModule { }
